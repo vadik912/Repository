51ef6b9fce7e40992718cf4167b1619090922256
Elnobel <nikitaunmortal@gmail.com>
2016-10-05 12:48:00 +0300

    "While" add

--- FSM/src/IncomeData.java
+++ FSM/src/IncomeData.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
 
 /**
  * Created by Numl on 04.10.2016.
  */
 public class IncomeData {
     private final String text;
-    /*Scanner sc = new Scanner(System.in);
-    private String text = sc.nextLine(); //текст для обработки*/
+
 
     public IncomeData(String text) {
         this.text = text;
--- FSM/src/MainRun.java
+++ FSM/src/MainRun.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
  * Created by Numl on 04.10.2016.
  */
 public class MainRun {
-
     public static void main(String[] args) {
-        FSM handle = new FSM();
-        System.out.println("Enter data");
-        Scanner sc = new Scanner(System.in);
-        String data = sc.nextLine();
-        IncomeData fsmdata = new IncomeData(data);
+        final FSM fsm = new FSM();
+        final Scanner sc = new Scanner(System.in);
+        final IncomeData data = new IncomeData(sc.nextLine());
 
-        handle.handlingData(fsmdata);
-        System.out.println("State of FSM after check: " + handle.getState());
+        while (!"End".equals(data)) {
+            System.out.println("Enter data");
 
-        handle.reset();
+
+
+            fsm.handlingData(data);
+
+            System.out.println("State of FSM after check: " + fsm.getState());
+
+            fsm.reset();
+        }
     }
 }
